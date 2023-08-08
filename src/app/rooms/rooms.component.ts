a58a1c16a93a405ea2fa276ab06282ef2f6bea3b
Rosie <rosie.bradshaw3@gmail.com>
2023-08-08 20:26:29 +0200

    room component created and practise with toggle rooms into view

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Room } from './rooms';
+
+@Component({
+  selector: 'hotelinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+  hotelName = "Hilton Hotel"; 
+  numberOfRooms = 10;
+  hideRooms= false;
+  rooms: Room = {
+    totalRooms:20,
+    availableRooms: 10, 
+    bookedRooms: 5
+  };
+  toggle() {
+    this.hideRooms = !this.hideRooms
+  }
+}
