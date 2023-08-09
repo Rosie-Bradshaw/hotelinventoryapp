a7a4f41ec5a136ef16509a208584c71ab80859b1
Rosie <rosie.bradshaw3@gmail.com>
2023-08-09 15:03:46 +0200

    practice with directives ngIf, ngFor, ngSwitch, ngClass, ngStyle and various pipes

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hotelinv-rooms',
@@ -15,6 +15,38 @@ export class RoomsComponent {
     availableRooms: 10, 
     bookedRooms: 5
   };
+
+  roomList: RoomList[] = [{
+    roomNumber: 1, 
+    roomType: 'Deluxe Room', 
+    amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen', 
+    price: 500, 
+    photos: 'https://unsplash.com/photos/AH8zKXqFITA',
+    checkinTime: new Date(11-11-2023),
+    checkoutTime: new Date(12-11-2023), 
+    rating: 4.5
+  }, 
+  {
+    roomNumber: 2, 
+    roomType: 'Deluxe Room', 
+    amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen', 
+    price: 1000, 
+    photos: 'https://unsplash.com/photos/AH8zKXqFITA',
+    checkinTime: new Date(11-11-2023),
+    checkoutTime: new Date(12-11-2023), 
+    rating: 3.446
+  }, 
+  {
+    roomNumber: 3, 
+    roomType: 'Private Suite', 
+    amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen', 
+    price: 1500, 
+    photos: 'https://unsplash.com/photos/AH8zKXqFITA',
+    checkinTime: new Date(11-11-2023),
+    checkoutTime: new Date(12-11-2023), 
+    rating: 2.6
+  }
+]
   toggle() {
     this.hideRooms = !this.hideRooms
   }
